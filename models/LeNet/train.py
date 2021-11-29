1e6c54d84d3fdd16d7f3fe8825c1c6de3345430f
p3basta <p3basta@datasci.cs.uwaterloo.ca>
2021-11-28 20:23:40 -0500

    add LeNet training

--- models/LeNet/train.py
+++ models/LeNet/train.py
@@ -1,6 +1,7 @@
 # https://github.com/icpm/pytorch-cifar10
 
 import os, ssl
+import json
 
 if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
 getattr(ssl, '_create_unverified_context', None)):
@@ -16,23 +17,23 @@ import numpy as np
 
 import argparse
 
-from models import AlexNet, LeNet
-from misc import progress_bar
+from LeNet import LeNet
 
 CLASSES = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 def main():
     parser = argparse.ArgumentParser(description="cifar-10 with PyTorch")
     parser.add_argument('--lr', default=0.001, type=float, help='learning rate')
-    parser.add_argument('--epoch', default=50, type=int, help='number of epochs tp train for')
+    parser.add_argument('--epoch', default=1, type=int, help='number of epochs tp train for')
     parser.add_argument('--trainBatchSize', default=100, type=int, help='training batch size')
     parser.add_argument('--testBatchSize', default=100, type=int, help='testing batch size')
     parser.add_argument('--cuda', default=torch.cuda.is_available(), type=bool, help='whether cuda is in use')
     args = parser.parse_args()
 
     solver = Solver(args)
-    solver.run()
-
+    accuracy = solver.run()
+    output = {'accuracy' : accuracy}
+    print(json.dumps(output))
 
 class Solver(object):
     def __init__(self, config):
@@ -52,9 +53,9 @@ class Solver(object):
     def load_data(self):
         train_transform = transforms.Compose([transforms.RandomHorizontalFlip(), transforms.ToTensor()])
         test_transform = transforms.Compose([transforms.ToTensor()])
-        train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=train_transform)
+        train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=train_transform)
         self.train_loader = torch.utils.data.DataLoader(dataset=train_set, batch_size=self.train_batch_size, shuffle=True)
-        test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=test_transform)
+        test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=test_transform)
         self.test_loader = torch.utils.data.DataLoader(dataset=test_set, batch_size=self.test_batch_size, shuffle=False)
 
     def load_model(self):
@@ -63,15 +64,13 @@ class Solver(object):
             cudnn.benchmark = True
         else:
             self.device = torch.device('cpu')
-
-        self.model = LeNet.LeNet()
+        self.model = LeNet().to(self.device)
         
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
         self.scheduler = optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=[75, 150], gamma=0.5)
         self.criterion = nn.CrossEntropyLoss().to(self.device)
 
     def train(self):
-        print("train:")
         self.model.train()
         train_loss = 0
         train_correct = 0
@@ -86,18 +85,11 @@ class Solver(object):
             self.optimizer.step()
             train_loss += loss.item()
             prediction = torch.max(output, 1)  # second param "1" represents the dimension to be reduced
-            total += target.size(0)
-
-            # train_correct incremented by one if predicted right
             train_correct += np.sum(prediction[1].cpu().numpy() == target.cpu().numpy())
-
-            # progress_bar(batch_num, len(self.train_loader), 'Loss: %.4f | Acc: %.3f%% (%d/%d)'
-            #              % (train_loss / (batch_num + 1), 100. * train_correct / total, train_correct, total))
 
         return train_loss, train_correct / total
 
     def test(self):
-        print("test:")
         self.model.eval()
         test_loss = 0
         test_correct = 0
@@ -113,31 +105,24 @@ class Solver(object):
                 total += target.size(0)
                 test_correct += np.sum(prediction[1].cpu().numpy() == target.cpu().numpy())
 
-                # progress_bar(batch_num, len(self.test_loader), 'Loss: %.4f | Acc: %.3f%% (%d/%d)'
-                #              % (test_loss / (batch_num + 1), 100. * test_correct / total, test_correct, total))
-
         return test_loss, test_correct / total
 
     def save(self):
         model_out_path = "model.pth"
         torch.save(self.model, model_out_path)
-        print("Checkpoint saved to {}".format(model_out_path))
 
     def run(self):
         self.load_data()
         self.load_model()
         accuracy = 0
         for epoch in range(1, self.epochs + 1):
-            self.scheduler.step(epoch)
-            print("\n===> epoch: %d/200" % epoch)
             train_result = self.train()
-            print(train_result)
+            self.scheduler.step()
             test_result = self.test()
             accuracy = max(accuracy, test_result[1])
             if epoch == self.epochs:
-                print("===> BEST ACC. PERFORMANCE: %.3f%%" % (accuracy * 100))
                 self.save()
-
-
+        return accuracy
 if __name__ == '__main__':
     main()
+
