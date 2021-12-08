abd211f6fdd84ac5cfe556fb93db3f0909ee115e
p3basta <p3basta@datasci.cs.uwaterloo.ca>
2021-12-07 20:16:09 -0500

    intial MNIST training

--- models/MNIST/train.py
+++ models/MNIST/train.py
@@ -44,7 +44,18 @@ supported_models = {
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
+        #data, target = data.to(device), target.to(device)
+        
+        if args.parallelism == 'dp':
+            data = data.cuda(args.gpu, non_blocking=True)
+            target = target.cuda(args.gpu, non_blocking=True)
+        elif args.parallelism == 'mp':
+            data = data.cuda(1, non_blocking=True)
+            target = target.cuda(1, non_blocking=True)
+        elif args.parallelism == 'gpipe':
+            data = data.to(model.devices[0], non_blocking=True)
+            target = target.to(model.devices[-1], non_blocking=True)
+        
         optimizer.zero_grad()
         output = model(data)
         loss = F.nll_loss(output, target)
@@ -58,13 +69,24 @@ def train(args, model, device, train_loader, optimizer, epoch):
                 break
 
 
-def test(model, device, test_loader):
+def test(model, device, test_loader, args):
     model.eval()
     test_loss = 0
     correct = 0
     with torch.no_grad():
         for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
+            #data, target = data.to(device), target.to(device)
+            
+            if args.parallelism == 'dp':
+                data = data.cuda(args.gpu, non_blocking=True)
+                target = target.cuda(args.gpu, non_blocking=True)
+            elif args.parallelism == 'mp':
+                data = data.cuda(1, non_blocking=True)
+                target = target.cuda(1, non_blocking=True)
+            elif args.parallelism == 'gpipe':
+                data = data.to(model.devices[0], non_blocking=True)
+                target = target.to(model.devices[-1], non_blocking=True)
+
             output = model(data)
             test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
@@ -111,6 +133,9 @@ def init_args():
     parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
     args = parser.parse_args()
+    
+    args.gpu = None
+    
     return args
 
 def main():
@@ -128,9 +153,9 @@ def main():
     start_time = timer()
 
     if args.parallelism == "dp":
-        mp.spawn(main_worker, nprocs=ngpus_per_node, args=(device, args, best_accuracy))
+        mp.spawn(main_worker, nprocs=ngpus_per_node, args=(ngpus_per_node, device, args, best_accuracy))
     else:
-        main_worker(ngpus_per_node, device, args, best_accuracy)
+        main_worker(args.gpu, ngpus_per_node, device, args, best_accuracy)
 
     end_time = timer()
 
@@ -138,7 +163,8 @@ def main():
     print(json.dumps(reported_stats))
 
     
-def main_worker(ngpus_per_node, device, args, best_accuracy):
+def main_worker(gpu, ngpus_per_node, device, args, best_accuracy):
+    args.gpu = gpu
 
     train_kwargs = {'batch_size': args.batch_size}
     test_kwargs = {'batch_size': args.test_batch_size}
@@ -170,11 +196,11 @@ def main_worker(ngpus_per_node, device, args, best_accuracy):
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch)
-        test(model, device, test_loader)
+        test(model, device, test_loader, args)
         scheduler.step()
 
-    if args.save_model:
-        torch.save(model.state_dict(), "mnist_cnn.pt")
+    #if args.save_model:
+    #    torch.save(model.state_dict(), "mnist_cnn.pt")
 
 
 if __name__ == '__main__':
